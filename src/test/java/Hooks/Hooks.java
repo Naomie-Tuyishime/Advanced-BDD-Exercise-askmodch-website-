f397fa19d9a59af3a63bcb1e0b055aa4c37ced5d
uwase sophie <uwasesophie91@gmail.com>
2026-01-09 17:47:53 +0200

    removed comments

--- src/test/java/Hooks/Hooks.java
+++ src/test/java/Hooks/Hooks.java
@@ -7,10 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Hooks {
-<<<<<<< HEAD
 
-=======
->>>>>>> main
     public static WebDriver driver;
 
     public Hooks(){
